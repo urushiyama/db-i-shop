c6c99ba23d7847738a6915962b90e31c80cbd6e0
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-05 14:22:25 +0900

    Add logout page

---
+++ src/_logout-page.php
@@ -0,0 +1,17 @@
+<div class="box-login-form">
+  <div class="box-login-form-title">
+    ログアウトの確認
+  </div>
+  <div class="box-login-form-content">
+    <form class="box-content-column">
+      <div class="box-content-row">
+        <p>ログアウトしますか？</p>
+      </div>
+      <div class="box-content-row">
+        <input type="button" name="login" value="ログアウト">
+        <p> </p>
+        <input type="button" name="back" value="戻る">
+      </div>
+    </form>
+  </div>
+</div>
