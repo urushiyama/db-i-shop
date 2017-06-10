93e09ba620d0793591ed6683f80db8a869d0e4d7
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-11 00:17:00 +0900

    Fix page title

--- site/view_config.php
+++ site/view_config.php
@@ -1,11 +1,16 @@
 <?php
-$system_name = 'URIKAI';
+function system_name() {
+  return 'URIKAI';
+}
 
 function page_title($page_name) {
+  $system_name = system_name();
   if ($page_name == '') {
     return $system_name;
   } else {
     return "$page_name | $system_name";
   }
 }
+
+$system_name = system_name();
 ?>
