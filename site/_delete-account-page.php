a726a7458a8e583d1f51544fdcfa15e775e090f1
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-10 23:17:52 +0900

    Add delete account confirmation page

---
+++ site/_delete-account-page.php
@@ -0,0 +1,23 @@
+<div class="box-login-form">
+  <div class="box-login-form-title">
+    登録情報削除の確認
+  </div>
+  <div class="box-login-form-content">
+    <form method="post" action="?p=top&a=delete-account" class="box-content-column">
+      <div class="box-content-row">
+        <b>本当に登録情報を削除しますか？この操作は取り消せません。</b>
+      </div>
+      <div class="box-content-row">
+        <ul>
+          <li>今までの購入履歴や検索履歴は全て消去され、復元できなくなります。</li>
+          <li>削除したアカウントでは2度とログインできなくなります。</li>
+        </ul>
+      </div>
+      <div class="box-content-row">
+        <input type="submit" name="delete-account" value="登録情報を削除する">
+        <p> </p>
+        <input type="button" name="back" value="キャンセル">
+      </div>
+    </form>
+  </div>
+</div>
