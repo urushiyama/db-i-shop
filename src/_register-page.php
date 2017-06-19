6eb970b4eca06b5a144f69b02dbd55fc76ef1d22
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-20 01:50:41 +0900

    Revert "Add user friendly input preserved form"
    This reverts commit 01be64b6fcfed39378b3f75001b4a03faff8d871.

--- src/_register-page.php
+++ src/_register-page.php
@@ -1,11 +1,6 @@
 <?php
 $login_type = isset($_GET['login-as']) ? htmlspecialchars($_GET['login-as'])
                                     : SessionController::LOGIN_TYPE_MEMBER;
-if (isset($_POST['login_type'])) $login_type = htmlspecialchars($_POST['login_type']);
-$user_name = isset($_POST['user_name']) ? htmlspecialchars($_POST['user_name'])
-                                    : '';
-$password = isset($_POST['password']) ? htmlspecialchars($_POST['password'])
-                                    : '';
  ?>
 <div class="box-login-form">
   <div class="box-login-form-title">
@@ -17,11 +12,11 @@ $password = isset($_POST['password']) ? htmlspecialchars($_POST['password'])
       <input type="hidden" name="login_type" value="<?=$login_type ?>">
       <div class="box-content-row">
         <p>ユーザ名</p>
-        <input type="text" name="user_name" placeholder="ユーザ名" value="<?=$user_name ?>">
+        <input type="text" name="user_name" placeholder="ユーザ名">
       </div>
       <div class="box-content-row">
         <p>パスワード</p>
-        <input type="password" name="password" placeholder="パスワード" value="<?=$password ?>">
+        <input type="password" name="password" placeholder="パスワード">
       </div>
       <div class="box-content-row">
         <p>パスワードの再入力</p>
