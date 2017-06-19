ee5045607e2220fa022300a17fa49bf4855c4e13
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-19 14:26:37 +0900

    Add register, login, logout, delete account actions

--- src/_C_ActionDispatcher.php
+++ src/_C_ActionDispatcher.php
@@ -1,6 +1,7 @@
 <?php
 require_once '_C_SessionController.php';
 require_once '_C_ApplicationException.php';
+require_once '_C_Members.php';
 
 class ActionDispatcher {
 
@@ -31,12 +32,88 @@ class ActionDispatcher {
     #   - user_name : text
     #   - password : password
     #   - password-confirmation : password
+    #   - login_type : hidden
+    if (!isset($_POST['user_name'])
+        || !isset($_POST['password'])
+        || !isset($_POST['password-confirmation'])
+        || !isset($_POST['login_type'])) {
+      ApplicationException::create(ApplicationException::INVALID_OPERATION);
+      return false;
+    }
+    if (!array_key_exists($_POST['login_type'], SessionController::LOGIN_TYPE)) {
+      ApplicationException::create(ApplicationException::INVALID_OPERATION);
+      return false;
+    }
+    if ($_POST['password'] !== $_POST['password-confirmation']) {
+      ApplicationException::create(ApplicationException::FAILED_PASSWORD_CONFIRMATION);
+    }
+    if ($_POST['user_name'] == '') {
+      ApplicationException::create(ApplicationException::EMPTY_USER_NAME);
+    }
+    $user_name = str_replace([' ', '　', "\n", "\t"], '', $_POST['user_name']);
+    if ($user_name == '') {
+      ApplicationException::create(ApplicationException::INVISIBLE_USER_NAME);
+    }
+    if ($_POST['password'] == '') {
+      ApplicationException::create(ApplicationException::EMPTY_PASSWORD);
+    }
+    $model = SessionController::LOGIN_TYPE[$_POST['login_type']]['model'];
+    include 'config.php';
+    $model::setConnectionInfo([
+      'host'=>$dbserver,
+      'dbname'=>$dbname,
+      'user'=>$user,
+      'password'=>$password
+    ]);
+    $model::initDb();
+    $user = $model::find_by(['name'=>$_POST['user_name']]);
+    if ($user) {
+      ApplicationException::create(ApplicationException::DUPLICATED_USER_NAME);
+    }
+    if (ApplicationException::isStored()) return false;
+    /* Succeed for registration */
+    if (strlen($_POST['password']) < 6) {
+      ApplicationException::create(ApplicationException::SHORT_PASSWORD);
+    }
+    $new = Members::create(['name'=>$_POST['user_name'], 'password'=>$_POST['password']]);
+    SessionController::login($new, $_POST['login_type']);
   }
 
   static function loginAccount(MainController $con) {
     # - params
     #   - user_name : text
     #   - password : password
+    #   - login_type : hidden
+    if (!isset($_POST['user_name'])
+        || !isset($_POST['password'])
+        || !isset($_POST['login_type'])) {
+      ApplicationException::create(ApplicationException::INVALID_OPERATION);
+      return false;
+    }
+    if (!array_key_exists($_POST['login_type'], SessionController::LOGIN_TYPE)) {
+      ApplicationException::create(ApplicationException::INVALID_OPERATION);
+      return false;
+    }
+    $model = SessionController::LOGIN_TYPE[$_POST['login_type']]['model'];
+    include 'config.php';
+    $model::setConnectionInfo([
+      'host'=>$dbserver,
+      'dbname'=>$dbname,
+      'user'=>$user,
+      'password'=>$password
+    ]);
+    $model::initDb();
+    $user = $model::find_by(['name'=>$_POST['user_name']]);
+    if (!$user) {
+      ApplicationException::create(ApplicationException::INVALID_LOGIN_COMBINATION);
+      return false;
+    }
+    if (!$user->authenticate($_POST['password'])) {
+      ApplicationException::create(ApplicationException::INVALID_LOGIN_COMBINATION);
+      return false;
+    }
+    SessionController::login($user, $_POST['login_type']);
+    return true;
   }
 
   static function updateAccount(MainController $con) {
@@ -48,11 +125,27 @@ class ActionDispatcher {
 
   static function logoutAccount(MainController $con) {
     # - no params
-    
+    SessionController::logout();
   }
 
   static function deleteAccount(MainController $con) {
     # - no params
+    $user = SessionController::currentUser();
+    if ($user == null) {
+      ApplicationException::create(ApplicationException::INVALID_OPERATION);
+      return;
+    }
+    include 'config.php';
+    $model = get_class($user);
+    $model::setConnectionInfo([
+      'host'=>$dbserver,
+      'dbname'=>$dbname,
+      'user'=>$user,
+      'password'=>$password
+    ]);
+    $model::initDb();
+    $user->destroy();
+    SessionController::destroy();
   }
 }
  ?>
