40fd1fd4b2e588499fa97d89eb8d2d1100b13cbb
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-20 01:58:07 +0900

    Add user friendly input-preserved form

--- src/_register-page.php
+++ src/_register-page.php
@@ -1,6 +1,12 @@
 <?php
 $login_type = isset($_GET['login-as']) ? htmlspecialchars($_GET['login-as'])
                                     : SessionController::LOGIN_TYPE_MEMBER;
+if (isset($_POST['login_type'])) $login_type = htmlspecialchars($_POST['login_type']);
+$user_name = isset($_POST['user_name']) ? htmlspecialchars($_POST['user_name'])
+                                    : '';
+$password = isset($_POST['password']) ? htmlspecialchars($_POST['password'])
+                                    : '';
+
  ?>
 <div class="box-login-form">
   <div class="box-login-form-title">
@@ -12,11 +18,11 @@ $login_type = isset($_GET['login-as']) ? htmlspecialchars($_GET['login-as'])
       <input type="hidden" name="login_type" value="<?=$login_type ?>">
       <div class="box-content-row">
         <p>ユーザ名</p>
-        <input type="text" name="user_name" placeholder="ユーザ名">
+        <input type="text" name="user_name" placeholder="ユーザ名" value="<?=$user_name ?>">
       </div>
       <div class="box-content-row">
         <p>パスワード</p>
-        <input type="password" name="password" placeholder="パスワード">
+        <input type="password" name="password" placeholder="パスワード" value="<?=$password ?>">
       </div>
       <div class="box-content-row">
         <p>パスワードの再入力</p>
