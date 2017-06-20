20ec66ec1834c6681171e721437684660d28e1cb
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-20 13:38:00 +0900

    Add advanced & admin search

--- site/_search-product-container.php
+++ site/_search-product-container.php
@@ -1,11 +1,36 @@
-<div class="search-form">
-  <div class="search-form-title">
+<div class="box-login-form">
+  <div class="box-login-form-title">
     商品を探す
   </div>
   <div class="search-form-content">
-    <form>
-      <input type="text" name="query" placeholder="商品名を入力して検索">
-      <input type="submit">
+    <form class="box-content-column">
+      <div class="box-content-row">
+        <input type="text" name="query" placeholder="商品名を入力して検索">
+        <input type="submit">
+      </div>
     </form>
+    <? if ($advanced_search): ?>
+    <div class="box-content-column">
+      <p>条件で絞り込む</p>
+    </div>
+    <div class="box-content-column">
+      <div class="box-content-row">
+        <input type="number" name="minimum-value" placeholder="下限">
+        <p>円以上</p>
+        <input type="number" name="maximum-value" placeholder="上限">
+        <p>円未満</p>
+      </div>
+    </div>
+    <? endif ?>
+    <? if ($admin): ?>
+    <div class="box-content-column">
+      <div class="box-content-row">
+        <input type="checkbox" name="show-banned" value="1" checked="checked">
+        <p>出品停止中の商品を表示</p>
+        <input type="checkbox" name="show-not-banned" value="1" checked="checked">
+        <p>出品継続中の商品を表示</p>
+      </div>
+    </div>
+    <? endif ?>
   </div>
 </div>
