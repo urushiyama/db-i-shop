1bdb00c1df695280fd9a3387400f802cb85d9413
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-22 00:20:19 +0900

    Add pagination in product search

--- site/_search-result-container.php
+++ site/_search-result-container.php
@@ -2,7 +2,9 @@
 require_once '_C_Renderer.php';
 
 $renderer = new Renderer('_not-found-page.php');
-
+$start_from = (isset($_GET['start'])) ? $_GET['start'] : 1;
+$query = (isset($_GET['query'])) ? urlencode(htmlspecialchars($_GET['query'])) : '';
+$max = 100; // 検索結果の個数
  ?>
 <div class="box-login-form">
   <div class="box-login-form-title">
@@ -11,5 +13,15 @@ $renderer = new Renderer('_not-found-page.php');
   <div class="box-login-form-content">
     <?=$renderer->render(['template'=>'_brief-product-container.php']) ?>
     <?=$renderer->render(['template'=>'_brief-product-container.php', 'product_name'=>'Lorem Keeper', 'dealer_name'=>'Foo Bar Co.Ltd.', 'product_price'=>14860]) ?>
+    <div class="box-content-column">
+      <div class="box-content-row">
+        <? if ($start_from > 10): ?>
+        <input type="button" value="前の10件" onclick="?p=search-product&amp;query=<?=$query ?>&amp;start=<?=$start_from - 10 ?>">
+        <? endif ?>
+        <? if ($start_from+10 <= $max): ?>
+        <input type="button" value="次の10件" onclick="?p=search-product&amp;query=<?=$query ?>&amp;start=<?=$start_from + 10 ?>">
+        <? endif ?>
+      </div>
+    </form>
   </div>
 </div>
