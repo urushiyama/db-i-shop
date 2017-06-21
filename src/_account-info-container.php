3e91c35395e7bc55782fcbca765ba00f0e54fad8
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-22 00:25:53 +0900

    Fix form style

--- src/_account-info-container.php
+++ src/_account-info-container.php
@@ -9,7 +9,7 @@ $user = SessionController::currentUser();
     <h2><?= htmlspecialchars($user->name) ?>さんの会員情報</h2>
   </div>
   <div class="box-login-form-content">
-    <form>
+    <form class="box-content-row">
       <input type="button" name="login" value="会員情報の更新/削除" onclick="location.href='?p=register-update'">
       <p>または</p>
       <input type="button" name="register" value="ログアウト" onclick="location.href='?p=logout'">
--- src/_login-reg-container.php
+++ src/_login-reg-container.php
@@ -6,7 +6,7 @@ if (!isset($login_as)) $login_as = SessionController::LOGIN_TYPE_MEMBER;
     <h2><?= SessionController::LOGIN_TYPE[$login_as]['name'] ?>としてログイン・新規登録する</h2>
   </div>
   <div class="box-login-form-content">
-    <form>
+    <form class="box-content-row">
       <input type="button" name="login" value="ログイン" onclick="location.href='?p=login&amp;login-as=<?=urlencode($login_as) ?>'">
       <p>または</p>
       <input type="button" name="register" value="新規登録" onclick="location.href='?p=register&amp;login-as=<?=urlencode($login_as) ?>'">
