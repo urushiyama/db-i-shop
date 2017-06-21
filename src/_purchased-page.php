241b31758f90215dcd439827b959ff5bc735002c
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-22 01:34:54 +0900

    Fix layout

--- src/_purchased-page.php
+++ src/_purchased-page.php
@@ -5,7 +5,7 @@
   <div class="box-login-form-content">
     <form method="post" action="./?p=top" class="box-content-column">
       <input type="hidden" name="a" value="logout-account">
-      <div class="box-content-row">
+      <div class="box-content-column">
         <p>ご購入いただきありがとうございます。購入手続きが完了しました。</p>
         <p>注文番号: xxxxxxxxxxxxx</p>
       </div>
