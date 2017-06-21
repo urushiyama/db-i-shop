b4480321982809c6cffc3d7a2ec9a704ef541b85
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-22 00:20:49 +0900

    Add purchased page view

---
+++ src/_purchased-page.php
@@ -0,0 +1,17 @@
+<div class="box-login-form">
+  <div class="box-login-form-title">
+    <h2>購入完了</h2>
+  </div>
+  <div class="box-login-form-content">
+    <form method="post" action="./?p=top" class="box-content-column">
+      <input type="hidden" name="a" value="logout-account">
+      <div class="box-content-row">
+        <p>ご購入いただきありがとうございます。購入手続きが完了しました。</p>
+        <p>注文番号: xxxxxxxxxxxxx</p>
+      </div>
+      <div class="box-content-row">
+        <input type="button" value="トップページへ" onclick="location.href='?p=top'">
+      </div>
+    </form>
+  </div>
+</div>
