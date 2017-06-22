12e0d64d700c308db00a69134ba32f8607bc85ca
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-22 10:44:34 +0900

    Move product_id input front

--- src/_brief-product-edit-button-container.php
+++ src/_brief-product-edit-button-container.php
@@ -6,8 +6,8 @@ if (!isset($product_dealer)) $product_dealer = 0;
  ?>
 <form action="." method="post" class="box-content-column" style="flex: 1 1 0;">
   <input type="hidden" name="a" value="edit-product">
+  <input type="hidden" name="product_id" value="<?=htmlspecialchars($product_id, ENT_QUOTES) ?>">
   <div class="box-content-row" style="margin: 0;">
-    <input type="hidden" name="product_id" value="<?=htmlspecialchars($product_id, ENT_QUOTES) ?>">
     <input class="minimum-width-input" type="number" name="units" value="1" min="1" max="<?=htmlspecialchars($product_stock, ENT_QUOTES)?>">
     <p>残り<?=htmlspecialchars($product_stock) ?>個</p>
   </div>
